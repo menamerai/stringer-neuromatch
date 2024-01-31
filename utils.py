555ea5fbd618c9d87e198cc33502631000a1a780
menamerai <rphananh@gmail.com>
2024-01-31 00:19:12 -0500

    DT layer trained (raw 32-bin angle)

---
+++ utils.py
@@ -0,0 +1,47 @@
+import os, requests
+import numpy as np
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
+def retrieve_data_orientation() -> np.array:
+    """
+    Retrieve Stringer et al. (2019) orientation data from OSF repository.
+
+    Returns:
+        dat (np.array): array of orientation data
+    """
+    fpath = "./data/stringer_orientations.npy"
+    url = "https://osf.io/ny4ut/download"
+
+    if not os.path.exists(fpath):
+        try:
+            r = requests.get(url)
+        except requests.ConnectionError:
+            print("!!! Failed to download data !!!")
+        else:
+            if r.status_code != requests.codes.ok:
+                print("!!! Failed to download data !!!")
+            else:
+                with open(fpath, "wb") as fid:
+                    fid.write(r.content)
+
+    dat = np.load('./data/stringer_orientations.npy', allow_pickle=True).item()
+    return dat
+
+def circular_encoding(angle: np.array) -> tuple[np.array, np.array]:
+    """
+    Encode an angle into a pair of coordinates using sine and cosine functions.
+    Angle should be in radians.
+
+    Args:
+        angle (float): angle in radians
+
+    Returns:
+        x, y (float): coordinates
+    """
+    x = np.cos(angle)
+    y = np.sin(angle)
+    return x, y
